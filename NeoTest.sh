56c3a6f16ea23b6f16be086a590f512dd5ac844b
Jens <>
2023-01-12 15:53:16 +0100

    files

--- NeoTest.sh
+++ NeoTest.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-OUTPUTFILE="out.csv"
 MAPPINGFOLDER='Mapper/'
 INPUTFOLDER='ExampleInput'
 TO="KldBK,ESCO,BIBBTOOLS"
+CONN="bolt://localhost:7687"
+USER="neo4j"
+PW="neo4j"
 
 for f in $INPUTFOLDER/*
 do
     python3 GenericTextReader/reader.py --file $f | \
     python3 SkillMiner/skillminer.py -t $TO -p $MAPPINGFOLDER | \
-    python3 NECSVWriter/necsvwriter.py -a -o $OUTPUTFILE
+    python3 NeoWriter/neowriter.py -c $CONN -u $USER -p $PW -d
 done
